a041c7f45479d84993835909d1995874c6df4411
Wahiddin Ishak <untuk_IT@live.com>
2020-06-21 00:59:51 +0700

    Add lib: moment.js. Create: CRUD Data.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,6 +50,79 @@ namespace crud_music.Controllers
             });
         }
 
+        public IActionResult addEdit(int id)
+        {
+            var model = new artist();
+
+            if (id != 0)
+            {
+                var data = _db.Artists.Find(id);
+                model.artisID = data.artisID;
+                model.artisName = data.artisName;
+                model.albumName = data.albumName;
+                model.imageURL = data.imageURL;
+                model.releaseDate = data.releaseDate;
+                model.price = data.price;
+                model.sampleURL = data.sampleURL;
+            }
+            return PartialView("addEdit", model);
+        }
+
+        public async Task<IActionResult> submitMusic(artist data)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dynamic dt;
+                    dt = data.artisID != 0 ? _db.Artists.FirstOrDefault(a => a.artisID == data.artisID) : new artist();
+                    dt.artisName = data.artisName;
+                    dt.albumName = data.albumName;
+                    dt.imageURL = data.imageURL;
+                    dt.releaseDate = data.releaseDate;
+                    dt.price = data.price;
+                    dt.sampleURL = data.sampleURL;
+
+                    if (data.artisID != 0)
+                    {
+                        _db.Artists.Update(dt);
+                    }
+                    else
+                    {
+                        _db.Artists.Add(dt);
+                    }
+
+                    await _db.SaveChangesAsync();
+
+                    return Content("OK");
+                }
+                catch (Exception ex)
+                {
+                    return Content(ex.Message.ToString());
+                }
+            }
+            return View(data);
+        }
+
+        public IActionResult playMUsic(int id)
+        {
+            return new JsonResult(_db.Artists.FindAsync(id));
+        }
+
+        public async Task<IActionResult> delete(int id)
+        {
+            try
+            {
+                var d = _db.Artists.FirstOrDefault(a => a.artisID == id);
+                _db.Artists.Remove(d);
+                await _db.SaveChangesAsync();
+                return Content("OK");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message.ToString());
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/artist.cs
+++ Models/artist.cs
@@ -10,11 +10,29 @@ namespace crud_music.Models
     {
         [Key]
         public int artisID { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Artis Name")]
         public string artisName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Album Name")]
         public string albumName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Image URL")]
         public string imageURL { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Date Release")]
         public DateTime releaseDate { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Price")]
         public double price { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Sample URL")]
         public string sampleURL { get; set; }
     }
 }
