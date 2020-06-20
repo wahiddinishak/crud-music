f48a51fc80192a1d44dc3878789c2805510f3c52
Wahiddin Ishak <untuk_IT@live.com>
2020-06-20 21:29:29 +0700

    cleansing layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,11 +22,7 @@ namespace crud_music.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+               
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
